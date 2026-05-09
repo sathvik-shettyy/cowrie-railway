a4e9bed8c4841f59394c4d91fbc97683609f1c43
Sathvik Shetty <74665830+sathvik-shettyy@users.noreply.github.com>
2026-05-09 20:55:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+echo "Starting Cowrie..."
+
+cd /opt/cowrie
+
+mkdir -p var/lib/cowrie
+mkdir -p var/log/cowrie
+
+touch var/log/cowrie/cowrie.json
+
+. cowrie-env/bin/activate
+
+bin/cowrie start
+
+echo "Cowrie started"
+
+cd /app
+
+echo "Starting dashboard..."
+
+python dashboard.py
