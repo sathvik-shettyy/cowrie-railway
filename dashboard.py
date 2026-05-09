e9107f12f448d057631ffa0b9eccb57e577c1061
Sathvik Shetty <74665830+sathvik-shettyy@users.noreply.github.com>
2026-05-09 22:40:42 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🐮 Cowrie is running"
+    return "🐮 Cowrie Dashboard Active"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
